1e8331f28de8438e100115d5470a32ddb0e004ec
yamabouz <zakiyamaiueo@gmail.com>
2019-08-19 12:01:00 +0000

    validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,11 @@
             </nav>
         </header>
         
+        @include('commons.navbar')
+        
         <div class="container">
+            @include('commons.error_messages')
+            
             @yield('content')
         </div>
         
